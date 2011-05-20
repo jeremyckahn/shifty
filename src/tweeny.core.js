2274e9dcd05a480f1850d73be1c34743ab2d46f3
Jeremy Kahn <jeremyckahn@gmail.com>
2011-05-19 22:31:56 -0500

    Some facade setup stuff.

--- src/tweeny.core.js
+++ src/tweeny.core.js
@@ -6,15 +6,46 @@
 	}
 	
 	global.tweeny = tweeny = {
-		fps: 30,
+		// The framerate at which Tweeny updates.
+		'fps': 30,
+		
+		// The default easing formula.  This can be changed publicly.
+		'easing': 'linear',
+		
+		// The default `duration`.  This can be changed publicly.
+		'duration': 500,
 		
 		/**
-		 * @param {Object} fromProps 
-		 * @param {Object} toProps
+		 * @param {Object} from 
+		 * @param {Object} to
 		 * @param {Number} duration
-		 * @param {String} easingFormula
+		 * @param {String} easing
 		 */
-		tween: function (fromProps, toProps, duration, easingFormula) {
+		'tween': function tween (from, to, duration, easing) {
+			var params,
+				step,
+				complete;
+			
+			if (toProps) {
+				// Assume the shorthand syntax is being used.
+				step = function () {};
+				complete = function () {};
+				from = params.from || {};
+				to = to || {};
+				duration = duration || this.duration;
+				easing = easing || this.easing;
+			} else {
+				params = fromProps;
+				
+				// If the second argument is not present, assume the longhand syntax is being used.
+				step = params.step || function () {};
+				complete = params.complete || function () {};
+				from = params.from || {};
+				to = params.to || {};
+				duration = params.duration || this.duration;
+				easing = params.easing || this.easing;
+			}
+			
 			
 		},
 		
@@ -28,7 +59,7 @@
 		 * @param c Change in value (delta)
 		 * @param d Duration of the tween
 		 */
-		formula : {
+		'formula' : {
 			linear: function (t, b, c, d) {
 				// no easing, no acceleration
 				return c * t / d + b;
