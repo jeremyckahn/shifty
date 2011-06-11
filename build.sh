34595d0a665ba8ce0883b6f5d4421418d430b56b
Jeremy Kahn <jeremyckahn@gmail.com>
2011-06-11 14:38:34 -0500

    Added a build script.

---
+++ build.sh
@@ -0,0 +1,24 @@
+#Shifty build script.  Cobbled together with help from: http://blog.badsyntax.co/post/1465134196/minifying-javascript-using-google-closure-compiler-and
+
+# All the files to build...
+cat src/shifty.core.js \
+	src/shifty.formulas.js \
+	src/shifty.queue.js \
+	src/shifty.color.js \
+	src/shifty.px.js \
+	> builds/shifty.full.js
+
+
+in=builds/shifty.full.js
+out=builds/shifty.min.js
+
+curl -s \
+        -d compilation_level=SIMPLE_OPTIMIZATIONS \
+	-d output_format=text \
+	-d output_info=compiled_code \
+	--data-urlencode "js_code@${in}" \
+	http://closure-compiler.appspot.com/compile \
+	 > $out
+
+# Remove the full concatenated script
+rm builds/shifty.full.js
