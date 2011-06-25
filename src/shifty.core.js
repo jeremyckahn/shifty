807b9a8abe8fe01acb68b7a834f2f9955fc32762
Jeremy Kahn <jeremyckahn@gmail.com>
2011-06-25 11:16:09 -0500

    Added Interpolate to the build script and made a dumb build validation test.  Incremented the core patch version.

--- src/shifty.core.js
+++ src/shifty.core.js
@@ -3,7 +3,7 @@
 /**
 Shifty - A teeny tiny tweening engine in JavaScript. 
 By Jeremy Kahn - jeremyckahn@gmail.com
-  v0.1.1
+  v0.1.2
 
 For instructions on how to use Shifty, please consult the README: https://github.com/jeremyckahn/tweeny/blob/master/README.md
 
