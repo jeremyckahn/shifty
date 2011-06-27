b64bbbecd72050e17af748390b4fccbf9914de60
Jeremy Kahn <jeremyckahn@gmail.com>
2011-06-26 22:04:01 -0500

    Added first version of the Simple Ease function.

---
+++ src/shifty.simple_ease.js
@@ -0,0 +1,28 @@
+/*global setTimeout:true, clearTimeout:true */
+
+/**
+Shifty Simple Ease Extension
+By Jeremy Kahn - jeremyckahn@gmail.com
+  v0.1.0
+
+Dependencies: shifty.core.js
+
+Tweeny and all official extensions are freely available under an MIT license.
+For instructions on how to use Tweeny and this extension, please consult the manual: https://github.com/jeremyckahn/tweeny/blob/master/README.md
+For instructions on how to use this extension, please see: https://github.com/jeremyckahn/shifty/blob/master/doc/shifty.queue.md
+
+MIT Lincense.  This code free to use, modify, distribute and enjoy.
+
+*/
+
+(function shiftySimpleEase (global) {
+	
+	global.Tweenable.util.simpleEase = function (easing, position) {
+		var easingMethod;
+		
+		easingMethod = Tweenable.prototype.formula[easing] || Tweenable.prototype.formula.linear;
+		
+		// Fake some values and return the result.
+		return easingMethod(position, 0, 1, 1)
+	};
+}(this));
