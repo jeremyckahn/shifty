6c529ddbc2812874f894ac244a7e0fbbc654dcce
Jeremy Kahn <jeremyckahn@gmail.com>
2011-07-12 15:44:04 -0700

    Added a simple Grow-n-Fade demo.

---
+++ demos/demo1/shiftme.js
@@ -0,0 +1,62 @@
+(function (global) {
+	var me
+		,tweener
+		,originalColor
+		,originalFontSize;
+	
+	function getStyle(el, style) {
+		return window.getComputedStyle(el).getPropertyCSSValue(style).cssText;
+	}
+
+	function grow (callback) {
+		tweener.to({
+			'to': {
+				'font-size': '200px'
+			}
+
+			,'duration': 600
+
+			,'easing': 'easeOutBounce'
+
+			,'step': function step () {
+				me.style.fontSize = this['font-size'];
+			}
+
+			,'callback': callback
+		});
+	}
+
+	function fade (callback) {
+		tweener.to({
+			'to': {
+				'color': '#bada55'
+			}
+
+			,'duration': 800
+
+			,'easing': 'easeOutExpo'
+
+			,'step': function step () {
+				me.style.color = this['color'];
+			}
+
+			,'callback': callback
+		});
+	}
+	
+	me = document.getElementById('shift-me');
+	originalFontSize = originalColor = getStyle(me, 'font-size');
+	originalColor = getStyle(me, 'color');
+
+	tweener = new Tweenable({
+		'initialState': {
+			'font-size': originalFontSize
+			,'color': originalColor
+		}
+	});
+	
+	grow(function () {
+		fade();
+	});
+	
+} (this));
