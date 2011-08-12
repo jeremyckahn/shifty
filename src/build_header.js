69e5d557f10a0f678788264419c20eaa3bf3b142
Jeremy Kahn <jeremyckahn@gmail.com>
2011-08-11 21:09:32 -0500

    Incremented the version to 0.4.3.

--- src/build_header.js
+++ src/build_header.js
@@ -2,7 +2,7 @@
 Shifty - A teeny tiny tweening engine in JavaScript. 
 By Jeremy Kahn - jeremyckahn@gmail.com
 
-  v0.4.2
+  v0.4.3
 
 For instructions on how to use Shifty, please consult the README: https://github.com/jeremyckahn/shifty/blob/master/README.md
 
