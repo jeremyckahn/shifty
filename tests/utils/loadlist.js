c40a6eb475a1e67a273729cf979c0a992046c7d0
Jeremy Kahn <jeremyckahn@gmail.com>
2011-09-24 08:44:07 -0700

    Added some test structure.

---
+++ tests/utils/loadlist.js
@@ -0,0 +1,15 @@
+(function (global) {
+  require([
+      '../src/shifty.core.js'
+      ,'../src/shifty.formulas.js'
+      ,'../src/shifty.color.js'
+      ,'../src/shifty.css_units.js'
+      ,'../src/shifty.queue.js'
+      ,'../src/shifty.interpolate.js'
+      ,'../src/shifty.simple_ease.js'
+      ,'../src/shifty.clamp.js'
+    ], function () {
+    
+    console.log('Loaded.');
+  });
+} (this));
