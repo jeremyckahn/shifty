3cfd8d7b4d3ea10de13620e8c9116e2808279c6d
Jeremy Kahn <jeremyckahn@gmail.com>
2011-11-25 13:22:39 -0600

    Converted tabs to spaces for the demos.

--- demos/demo1/shiftme.js
+++ demos/demo1/shiftme.js
@@ -1,87 +1,87 @@
 (function (global) {
-	var me
-		,tweener
-		,originalColor
-		,originalFontSize;
-	
-	function getStyle (el, style) {
-		var ret
-			,styleWords;
-		
-		if (window.getComputedStyle) {
-			ret = window.getComputedStyle(el).getPropertyCSSValue(style).cssText;
-		} else {
-			// For IE
-			styleWords = style.split('-');
-			if (styleWords.length === 2) {
-				// ugly!
-				style = styleWords[0];
-				style += styleWords[1].match(/^./)[0].toUpperCase();
-				style += styleWords[1].split('').slice(1).join('');
-			}
-			
-			ret = el.currentStyle[style];
-		}
-		
-		return ret;
-	}
+  var me
+    ,tweener
+    ,originalColor
+    ,originalFontSize;
+  
+  function getStyle (el, style) {
+    var ret
+      ,styleWords;
+    
+    if (window.getComputedStyle) {
+      ret = window.getComputedStyle(el).getPropertyCSSValue(style).cssText;
+    } else {
+      // For IE
+      styleWords = style.split('-');
+      if (styleWords.length === 2) {
+        // ugly!
+        style = styleWords[0];
+        style += styleWords[1].match(/^./)[0].toUpperCase();
+        style += styleWords[1].split('').slice(1).join('');
+      }
+      
+      ret = el.currentStyle[style];
+    }
+    
+    return ret;
+  }
 
-	function grow (el, size, callback) {
-		tweener.to({
-			'to': {
-				'font-size': size + 'px'
-			}
+  function grow (el, size, callback) {
+    tweener.to({
+      'to': {
+        'font-size': size + 'px'
+      }
 
-			,'duration': 600
+      ,'duration': 600
 
-			,'easing': 'easeOutBounce'
+      ,'easing': 'easeOutBounce'
 
-			,'step': function step () {
-				el.style.fontSize = this['font-size'];
-			}
+      ,'step': function step () {
+        el.style.fontSize = this['font-size'];
+      }
 
-			,'callback': callback
-		});
-	}
+      ,'callback': callback
+    });
+  }
 
-	function fade (el, callback) {
-		tweener.to({
-			'to': {
-				'color': '#001BB5'
-			}
+  function fade (el, callback) {
+    tweener.to({
+      'to': {
+        'color': '#001BB5'
+      }
 
-			,'duration': 800
+      ,'duration': 800
 
-			,'easing': 'easeOutExpo'
+      ,'easing': 'easeOutExpo'
 
-			,'step': function step () {
-				el.style.color = this['color'];
-			}
+      ,'step': function step () {
+        el.style.color = this['color'];
+      }
 
-			,'callback': callback
-		});
-	}
+      ,'callback': callback
+    });
+  }
 
-	function init (el) {
-		originalFontSize = getStyle(el, 'font-size');
-		originalColor = getStyle(el, 'color');
-		
-		tweener = new Tweenable({
-			'initialState': {
-				'font-size': originalFontSize
-				,'color': originalColor
-			}
+  function init (el) {
+    originalFontSize = getStyle(el, 'font-size');
+    originalColor = getStyle(el, 'color');
+    
+    tweener = new Tweenable({
+      'initialState': {
+        'font-size': originalFontSize
+        ,'color': originalColor
+      }
 
-			,'fps': 60
-		});
-	}
+      ,'fps': 60
+    });
+  }
 
-	global.shiftyDemo = global.shiftyDemo || {};
+  global.shiftyDemo = global.shiftyDemo || {};
 
-	Tweenable.util.simpleCopy(global.shiftyDemo, {
-	    'introInit': init
-	    ,'introGrow': grow
-	    ,'introFade': fade
-	});
-	
+  Tweenable.util.simpleCopy(global.shiftyDemo, {
+      'introInit': init
+      ,'introGrow': grow
+      ,'introFade': fade
+  });
+  
 } (this));
--- demos/demo2/helix.js
+++ demos/demo2/helix.js
@@ -1,99 +1,99 @@
 /*global window:true, setTimeout:true */
 (function (global) {
-	var CYCLE_SPEED = 1500
-		,HELIX_WIDTH = 300
-		,SEGMENT_BUFFER = 0.2
-		,startTime
-		,pausedAtTime
-		,segmentContainer
-		,segments
-		,stepStateLists
-		,updateHandle
-		,isPlaying
-		,i;
-	
-	function now () {
-		return +(new Date());
-	}
-	
-	function getPosition (loopPosition, startBuffer) {
-		var moddedLoopPosition
-			,interpolator
-			,interpolatedValue;
-		
-		if ((loopPosition - startBuffer) < 0) {
-			return 0;
-		}
-		
-		interpolator = moddedLoopPosition = ((loopPosition - startBuffer) % 2);
-		
-		if (moddedLoopPosition > 1) {
-			interpolator = moddedLoopPosition - 1;
-		}	
-		
-		interpolatedValue = global.Tweenable.util.interpolate({
-		    from: { 'left': 0 },
-		    to: { 'left': HELIX_WIDTH },
-		    position: interpolator,
-		    easing: 'easeInOutSine'
-		}).left;
-		
-		if (moddedLoopPosition > 1) {
-			return HELIX_WIDTH - interpolatedValue;
-		} else {
-			return interpolatedValue;
-		}
-	}
-	
-	function updateSegment (index, loopPosition) {
-		var segment
-			,calculated;
-		
-		segment = segments[index];
-		calculated = getPosition(loopPosition, index * SEGMENT_BUFFER);
-		segment.style.left = calculated + 'px';
-	}
-	
-	function updateSegments (startTime, callback) {
-		var i
-			,timeDelta
-			,normalizedTime;
-		
-		timeDelta = now() - startTime;
-		normalizedTime = timeDelta / CYCLE_SPEED;
-		
-		for (i = 0; i < segments.length; i++) {
-			updateSegment(i, normalizedTime);
-		}
-		
-		callback();
-	}
-	
-	function loop () {
-		updateHandle = setTimeout(function () {
-			updateSegments(startTime, loop);
-		}, 1000 / 60);
-	}
-	
-	function pause () {
-		if (isPlaying === false) {
-			return;
-		}
-		
-		isPlaying = false;
-		pausedAtTime = Tweenable.util.now();
-		clearTimeout(updateHandle);
-	}
-	
-	function play () {
-		if (isPlaying === true) {
-			return;
-		}
-		
-		isPlaying = true;
-		startTime = Tweenable.util.now() - (pausedAtTime - startTime);
-		loop();
-	}
+  var CYCLE_SPEED = 1500
+    ,HELIX_WIDTH = 300
+    ,SEGMENT_BUFFER = 0.2
+    ,startTime
+    ,pausedAtTime
+    ,segmentContainer
+    ,segments
+    ,stepStateLists
+    ,updateHandle
+    ,isPlaying
+    ,i;
+  
+  function now () {
+    return +(new Date());
+  }
+  
+  function getPosition (loopPosition, startBuffer) {
+    var moddedLoopPosition
+      ,interpolator
+      ,interpolatedValue;
+    
+    if ((loopPosition - startBuffer) < 0) {
+      return 0;
+    }
+    
+    interpolator = moddedLoopPosition = ((loopPosition - startBuffer) % 2);
+    
+    if (moddedLoopPosition > 1) {
+      interpolator = moddedLoopPosition - 1;
+    } 
+    
+    interpolatedValue = global.Tweenable.util.interpolate({
+        from: { 'left': 0 },
+        to: { 'left': HELIX_WIDTH },
+        position: interpolator,
+        easing: 'easeInOutSine'
+    }).left;
+    
+    if (moddedLoopPosition > 1) {
+      return HELIX_WIDTH - interpolatedValue;
+    } else {
+      return interpolatedValue;
+    }
+  }
+  
+  function updateSegment (index, loopPosition) {
+    var segment
+      ,calculated;
+    
+    segment = segments[index];
+    calculated = getPosition(loopPosition, index * SEGMENT_BUFFER);
+    segment.style.left = calculated + 'px';
+  }
+  
+  function updateSegments (startTime, callback) {
+    var i
+      ,timeDelta
+      ,normalizedTime;
+    
+    timeDelta = now() - startTime;
+    normalizedTime = timeDelta / CYCLE_SPEED;
+    
+    for (i = 0; i < segments.length; i++) {
+      updateSegment(i, normalizedTime);
+    }
+    
+    callback();
+  }
+  
+  function loop () {
+    updateHandle = setTimeout(function () {
+      updateSegments(startTime, loop);
+    }, 1000 / 60);
+  }
+  
+  function pause () {
+    if (isPlaying === false) {
+      return;
+    }
+    
+    isPlaying = false;
+    pausedAtTime = Tweenable.util.now();
+    clearTimeout(updateHandle);
+  }
+  
+  function play () {
+    if (isPlaying === true) {
+      return;
+    }
+    
+    isPlaying = true;
+    startTime = Tweenable.util.now() - (pausedAtTime - startTime);
+    loop();
+  }
 
         function init (containerId) {
             pausedAtTime = startTime = Tweenable.util.now();
@@ -102,16 +102,16 @@
             stepStateLists = [];
             
             for (i = 0; i < segments.length; i++) {
-                    stepStateLists.push([]);	
+                    stepStateLists.push([]);  
             }
             
             window.pause = pause;
             window.play = play;
         }
 
-	global.shiftyDemo = global.shiftyDemo || {};
+  global.shiftyDemo = global.shiftyDemo || {};
 
-	Tweenable.util.simpleCopy(global.shiftyDemo, {
+  Tweenable.util.simpleCopy(global.shiftyDemo, {
             'helixInit': init
             ,'helixPlay': play
             ,'helixPause': pause
