74ef566a9840518245bb558b61869057bd9fbd3c
Jeremy Kahn <jeremyckahn@gmail.com>
2012-05-02 23:02:08 -0700

    Changed all instances of Tweeny to Shifty.

--- dist/shifty.js
+++ dist/shifty.js
@@ -828,8 +828,8 @@ By Jeremy Kahn - jeremyckahn@gmail.com
 
 Dependencies: shifty.core.js
 
-Tweeny and all official extensions are freely available under an MIT license.
-For instructions on how to use Tweeny and this extension, please consult the manual: https://github.com/jeremyckahn/shifty/blob/master/README.md
+Shifty and all official extensions are freely available under an MIT license.
+For instructions on how to use Shifty and this extension, please consult the manual: https://github.com/jeremyckahn/shifty/blob/master/README.md
 For instructions on how to use this extension, please see: https://github.com/jeremyckahn/shifty/blob/master/doc/shifty.queue.md
 
 MIT Lincense.  This code free to use, modify, distribute and enjoy.
@@ -934,8 +934,8 @@ By Jeremy Kahn - jeremyckahn@gmail.com
 
 Dependencies: shifty.core.js
 
-Tweeny and all official extensions are freely available under an MIT license.
-For instructions on how to use Tweeny and this extension, please consult the manual: https://github.com/jeremyckahn/shifty/blob/master/README.md
+Shifty and all official extensions are freely available under an MIT license.
+For instructions on how to use Shifty and this extension, please consult the manual: https://github.com/jeremyckahn/shifty/blob/master/README.md
 For instructions on how to use this extension, please see: https://github.com/jeremyckahn/shifty/blob/master/doc/shifty.queue.md
 
 MIT Lincense.  This code free to use, modify, distribute and enjoy.
