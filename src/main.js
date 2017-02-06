4bdd8f6196b1015792315f686c0c2b42e6d3f896
Jeremy Kahn <jeremyckahn@gmail.com>
2017-02-06 08:32:14 -0600

    ES5-ify bezier code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import { Tweenable, shallowCopy } from './shifty.core';
 import { interpolate } from './shifty.interpolate';
+import { setBezierFunction, unsetBezierFunction } from './shifty.bezier';
 import formulas from './shifty.formulas';
 
 shallowCopy(Tweenable.prototype.formula, formulas);
 
-export default { Tweenable, interpolate };
+export default {
+  Tweenable,
+  interpolate,
+  setBezierFunction,
+  unsetBezierFunction
+};
