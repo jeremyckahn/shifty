902451b546c8c71dd513396afb91c8d6dd31a50f
Jeremy Kahn <jeremyckahn@gmail.com>
2017-02-08 08:13:36 -0600

    Remove broken import

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { Tweenable, shallowCopy } from './shifty.core';
 import { interpolate } from './shifty.interpolate';
 import { setBezierFunction, unsetBezierFunction } from './shifty.bezier';
-import formulas from './shifty.formulas';
 
 export default {
   Tweenable,
