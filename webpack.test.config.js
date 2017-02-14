9ef91f98633391d60cee3c3b512ae08d39d6a2bc
Jeremy Kahn <jeremyckahn@gmail.com>
2017-02-14 08:30:37 -0600

    Set up UI for Mocha

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,21 +1,18 @@
 const path = require('path');
 const Webpack = require('webpack');
 
-const { version } = require('./package.json');
-const isProduction = process.env.NODE_ENV === 'production';
-
 const modulePaths = [
   'scripts',
   path.join(__dirname, 'node_modules')
 ];
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './test/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/assets/',
-    filename: 'shifty.js',
-    library: 'shifty',
+    filename: 'index.js',
+    library: 'index',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
@@ -39,19 +36,7 @@ module.exports = {
     // http://webpack.github.io/docs/troubleshooting.html#npm-linked-modules-doesn-t-find-their-dependencies
     fallback: modulePaths
   },
-  plugins: [
-    new Webpack.optimize.UglifyJsPlugin({
-      compress: {
-        dead_code: true,
-        unused: true
-      },
-      output: {
-        comments: false
-      }
-    }),
-    new Webpack.BannerPlugin(version),
-    //new Webpack.DefinePlugin({
-      //SHIFTY_DEBUG_NOW: true
-    //})
-  ]
+  devServer: {
+    port: 9009
+  }
 };
