8e5f58cfc3b0e11ba45ba83f7da0e27cd46ea7cc
Jeremy Kahn <jeremyckahn@gmail.com>
2017-02-26 12:39:28 -0600

    Clean up shifty.easing-functions.js

--- src/shifty.easing-functions.js
+++ src/shifty.easing-functions.js
@@ -12,154 +12,80 @@
  *  Open source under the BSD License.
  *  Easing Equations (c) 2003 Robert Penner, all rights reserved.
  */
-export function linear (pos) {
-  return pos;
-}
+export const linear = pos => pos;
 
-export function easeInQuad (pos) {
-  return Math.pow(pos, 2);
-}
+export const easeInQuad = pos => Math.pow(pos, 2);
 
-export function easeOutQuad (pos) {
-  return -(Math.pow((pos - 1), 2) - 1);
-}
+export const easeOutQuad = pos => -(Math.pow((pos - 1), 2) - 1);
 
-export function easeInOutQuad (pos) {
-  if ((pos /= 0.5) < 1) {return 0.5 * Math.pow(pos,2);}
-  return -0.5 * ((pos -= 2) * pos - 2);
-}
+export const easeInOutQuad = pos =>
+  (pos /= 0.5) < 1 ?
+    0.5 * Math.pow(pos,2) :
+    -0.5 * ((pos -= 2) * pos - 2);
 
-export function easeInCubic (pos) {
-  return Math.pow(pos, 3);
-}
+export const easeInCubic = pos => Math.pow(pos, 3);
 
-export function easeOutCubic (pos) {
-  return (Math.pow((pos - 1), 3) + 1);
-}
+export const easeOutCubic = pos => Math.pow((pos - 1), 3) + 1;
 
-export function easeInOutCubic (pos) {
-  if ((pos /= 0.5) < 1) {return 0.5 * Math.pow(pos,3);}
-  return 0.5 * (Math.pow((pos - 2),3) + 2);
-}
+export const easeInOutCubic = pos =>
+  (pos /= 0.5) < 1 ?
+    0.5 * Math.pow(pos,3) :
+    0.5 * (Math.pow((pos - 2),3) + 2);
 
-export function easeInQuart (pos) {
-  return Math.pow(pos, 4);
-}
+export const easeInQuart = pos => Math.pow(pos, 4);
 
-export function easeOutQuart (pos) {
-  return -(Math.pow((pos - 1), 4) - 1);
-}
+export const easeOutQuart = pos => -(Math.pow((pos - 1), 4) - 1);
 
-export function easeInOutQuart (pos) {
-  if ((pos /= 0.5) < 1) {return 0.5 * Math.pow(pos,4);}
-  return -0.5 * ((pos -= 2) * Math.pow(pos,3) - 2);
-}
+export const easeInOutQuart = pos =>
+  (pos /= 0.5) < 1 ?
+    0.5 * Math.pow(pos,4) :
+    -0.5 * ((pos -= 2) * Math.pow(pos,3) - 2);
 
-export function easeInQuint (pos) {
-  return Math.pow(pos, 5);
-}
+export const easeInQuint = pos => Math.pow(pos, 5);
 
-export function easeOutQuint (pos) {
-  return (Math.pow((pos - 1), 5) + 1);
-}
+export const easeOutQuint = pos => Math.pow((pos - 1), 5) + 1;
 
-export function easeInOutQuint (pos) {
-  if ((pos /= 0.5) < 1) {return 0.5 * Math.pow(pos,5);}
-  return 0.5 * (Math.pow((pos - 2),5) + 2);
-}
+export const easeInOutQuint = pos =>
+  (pos /= 0.5) < 1 ?
+    0.5 * Math.pow(pos,5) :
+    0.5 * (Math.pow((pos - 2),5) + 2);
 
-export function easeInSine (pos) {
-  return -Math.cos(pos * (Math.PI / 2)) + 1;
-}
+export const easeInSine = pos => -Math.cos(pos * (Math.PI / 2)) + 1;
 
-export function easeOutSine (pos) {
-  return Math.sin(pos * (Math.PI / 2));
-}
+export const easeOutSine = pos => Math.sin(pos * (Math.PI / 2));
 
-export function easeInOutSine (pos) {
-  return (-0.5 * (Math.cos(Math.PI * pos) - 1));
-}
+export const easeInOutSine = pos => -0.5 * (Math.cos(Math.PI * pos) - 1);
 
-export function easeInExpo (pos) {
-  return (pos === 0) ? 0 : Math.pow(2, 10 * (pos - 1));
-}
+export const easeInExpo = pos => (pos === 0) ? 0 : Math.pow(2, 10 * (pos - 1));
 
-export function easeOutExpo (pos) {
-  return (pos === 1) ? 1 : -Math.pow(2, -10 * pos) + 1;
-}
+export const easeOutExpo = pos => (pos === 1) ? 1 : -Math.pow(2, -10 * pos) + 1;
 
-export function easeInOutExpo (pos) {
-  if (pos === 0) {return 0;}
-  if (pos === 1) {return 1;}
-  if ((pos /= 0.5) < 1) {return 0.5 * Math.pow(2,10 * (pos - 1));}
-  return 0.5 * (-Math.pow(2, -10 * --pos) + 2);
-}
-
-export function easeInCirc (pos) {
-  return -(Math.sqrt(1 - (pos * pos)) - 1);
-}
-
-export function easeOutCirc (pos) {
-  return Math.sqrt(1 - Math.pow((pos - 1), 2));
-}
-
-export function easeInOutCirc (pos) {
-  if ((pos /= 0.5) < 1) {return -0.5 * (Math.sqrt(1 - pos * pos) - 1);}
-  return 0.5 * (Math.sqrt(1 - (pos -= 2) * pos) + 1);
-}
-
-export function easeOutBounce (pos) {
-  if ((pos) < (1 / 2.75)) {
-    return (7.5625 * pos * pos);
-  } else if (pos < (2 / 2.75)) {
-    return (7.5625 * (pos -= (1.5 / 2.75)) * pos + 0.75);
-  } else if (pos < (2.5 / 2.75)) {
-    return (7.5625 * (pos -= (2.25 / 2.75)) * pos + 0.9375);
-  } else {
-    return (7.5625 * (pos -= (2.625 / 2.75)) * pos + 0.984375);
+export const easeInOutExpo = pos => {
+  if (pos === 0) {
+    return 0;
   }
-}
 
-export function easeInBack (pos) {
-  var s = 1.70158;
-  return (pos) * pos * ((s + 1) * pos - s);
-}
+  if (pos === 1) {
+    return 1;
+  }
 
-export function easeOutBack (pos) {
-  var s = 1.70158;
-  return (pos = pos - 1) * pos * ((s + 1) * pos + s) + 1;
-}
-
-export function easeInOutBack (pos) {
-  var s = 1.70158;
   if ((pos /= 0.5) < 1) {
-    return 0.5 * (pos * pos * (((s *= (1.525)) + 1) * pos - s));
+    return 0.5 * Math.pow(2,10 * (pos - 1));
   }
-  return 0.5 * ((pos -= 2) * pos * (((s *= (1.525)) + 1) * pos + s) + 2);
-}
 
-export function elastic (pos) {
-  return -1 * Math.pow(4,-8 * pos) * Math.sin((pos * 6 - 1) * (2 * Math.PI) / 2) + 1;
-}
+  return 0.5 * (-Math.pow(2, -10 * --pos) + 2);
+};
 
-export function swingFromTo (pos) {
-  var s = 1.70158;
-  return ((pos /= 0.5) < 1) ?
-      0.5 * (pos * pos * (((s *= (1.525)) + 1) * pos - s)) :
-      0.5 * ((pos -= 2) * pos * (((s *= (1.525)) + 1) * pos + s) + 2);
-}
+export const easeInCirc = pos => -(Math.sqrt(1 - (pos * pos)) - 1);
 
-export function swingFrom (pos) {
-  var s = 1.70158;
-  return pos * pos * ((s + 1) * pos - s);
-}
+export const easeOutCirc = pos => Math.sqrt(1 - Math.pow((pos - 1), 2));
 
-export function swingTo (pos) {
-  var s = 1.70158;
-  return (pos -= 1) * pos * ((s + 1) * pos + s) + 1;
-}
+export const easeInOutCirc = pos =>
+  ((pos /= 0.5) < 1) ?
+    -0.5 * (Math.sqrt(1 - pos * pos) - 1) :
+    0.5 * (Math.sqrt(1 - (pos -= 2) * pos) + 1);
 
-export function bounce (pos) {
+export const easeOutBounce = pos => {
   if (pos < (1 / 2.75)) {
     return (7.5625 * pos * pos);
   } else if (pos < (2 / 2.75)) {
@@ -169,9 +95,59 @@ export function bounce (pos) {
   } else {
     return (7.5625 * (pos -= (2.625 / 2.75)) * pos + 0.984375);
   }
-}
+};
 
-export function bouncePast (pos) {
+export const easeInBack = pos => {
+  const s = 1.70158;
+  return pos * pos * ((s + 1) * pos - s);
+};
+
+export const easeOutBack = pos => {
+  const s = 1.70158;
+  return (pos = pos - 1) * pos * ((s + 1) * pos + s) + 1;
+};
+
+export const easeInOutBack = pos => {
+  let s = 1.70158;
+  if ((pos /= 0.5) < 1) {
+    return 0.5 * (pos * pos * (((s *= (1.525)) + 1) * pos - s));
+  }
+  return 0.5 * ((pos -= 2) * pos * (((s *= (1.525)) + 1) * pos + s) + 2);
+};
+
+export const elastic = pos =>
+  -1 * Math.pow(4,-8 * pos) * Math.sin((pos * 6 - 1) * (2 * Math.PI) / 2) + 1;
+
+export const swingFromTo = pos => {
+  let s = 1.70158;
+  return ((pos /= 0.5) < 1) ?
+      0.5 * (pos * pos * (((s *= (1.525)) + 1) * pos - s)) :
+      0.5 * ((pos -= 2) * pos * (((s *= (1.525)) + 1) * pos + s) + 2);
+};
+
+export const swingFrom = pos => {
+  const s = 1.70158;
+  return pos * pos * ((s + 1) * pos - s);
+};
+
+export const swingTo = pos => {
+  const s = 1.70158;
+  return (pos -= 1) * pos * ((s + 1) * pos + s) + 1;
+};
+
+export const bounce = pos => {
+  if (pos < (1 / 2.75)) {
+    return (7.5625 * pos * pos);
+  } else if (pos < (2 / 2.75)) {
+    return (7.5625 * (pos -= (1.5 / 2.75)) * pos + 0.75);
+  } else if (pos < (2.5 / 2.75)) {
+    return (7.5625 * (pos -= (2.25 / 2.75)) * pos + 0.9375);
+  } else {
+    return (7.5625 * (pos -= (2.625 / 2.75)) * pos + 0.984375);
+  }
+};
+
+export const bouncePast = pos => {
   if (pos < (1 / 2.75)) {
     return (7.5625 * pos * pos);
   } else if (pos < (2 / 2.75)) {
@@ -181,17 +157,13 @@ export function bouncePast (pos) {
   } else {
     return 2 - (7.5625 * (pos -= (2.625 / 2.75)) * pos + 0.984375);
   }
-}
+};
 
-export function easeFromTo (pos) {
-  if ((pos /= 0.5) < 1) {return 0.5 * Math.pow(pos,4);}
-  return -0.5 * ((pos -= 2) * Math.pow(pos,3) - 2);
-}
+export const easeFromTo = pos =>
+  ((pos /= 0.5) < 1) ?
+    0.5 * Math.pow(pos,4) :
+    -0.5 * ((pos -= 2) * Math.pow(pos,3) - 2);
 
-export function easeFrom (pos) {
-  return Math.pow(pos,4);
-}
+export const easeFrom = pos => Math.pow(pos, 4);
 
-export function easeTo (pos) {
-  return Math.pow(pos,0.25);
-}
+export const easeTo = pos => Math.pow(pos, 0.25);
