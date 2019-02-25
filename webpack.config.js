84b030fe06f627ab3fa593eb06ae831e8d4416b4
Jeremy Kahn <jeremyckahn@gmail.com>
2019-02-25 08:22:08 -0600

    [WIP] Update webpack and babel dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const Webpack = require('webpack');
 const { version } = require('./package.json');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
@@ -27,16 +28,6 @@ module.exports = {
     modules: ['node_modules'],
   },
   plugins: [
-    new Webpack.optimize.UglifyJsPlugin({
-      compress: {
-        dead_code: true,
-        unused: true,
-      },
-      output: {
-        comments: false,
-      },
-      sourceMap: true,
-    }),
     new Webpack.BannerPlugin(version),
   ],
 };
