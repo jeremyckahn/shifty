8b14af362f91873867accd27c28c723488273ac8
Jeremy Kahn <jeremyckahn@gmail.com>
2023-06-08 09:47:28 -0500

    refactor(bezier): [#177] convert tests to TypeScript

--- src/bezier.test.ts
+++ src/bezier.test.ts
@@ -3,19 +3,35 @@ import {
   interpolate,
   setBezierFunction,
   unsetBezierFunction,
-} from './'
+} from '.'
+import { EasingKey } from './tweenable'
 
 test('can create a linear bezier easing curve', () => {
   setBezierFunction('bezier-linear', 0.25, 0.25, 0.75, 0.75)
 
   expect(interpolate({ x: 0 }, { x: 10 }, 0.25, 'linear').x.toFixed(1)).toEqual(
-    interpolate({ x: 0 }, { x: 10 }, 0.25, 'bezier-linear').x.toFixed(1)
+    interpolate(
+      { x: 0 },
+      { x: 10 },
+      0.25,
+      'bezier-linear' as EasingKey
+    ).x.toFixed(1)
   )
   expect(interpolate({ x: 0 }, { x: 10 }, 0.5, 'linear').x.toFixed(1)).toEqual(
-    interpolate({ x: 0 }, { x: 10 }, 0.5, 'bezier-linear').x.toFixed(1)
+    interpolate(
+      { x: 0 },
+      { x: 10 },
+      0.5,
+      'bezier-linear' as EasingKey
+    ).x.toFixed(1)
   )
   expect(interpolate({ x: 0 }, { x: 10 }, 0.75, 'linear').x.toFixed(1)).toEqual(
-    interpolate({ x: 0 }, { x: 10 }, 0.75, 'bezier-linear').x.toFixed(1)
+    interpolate(
+      { x: 0 },
+      { x: 10 },
+      0.75,
+      'bezier-linear' as EasingKey
+    ).x.toFixed(1)
   )
 })
 
@@ -23,26 +39,35 @@ test('can create a "stretched" linear bezier easing curve', () => {
   setBezierFunction('bezier-stretched-linear', 0, 0, 1, 1)
 
   expect(interpolate({ x: 0 }, { x: 10 }, 0.25, 'linear').x.toFixed(1)).toEqual(
-    interpolate({ x: 0 }, { x: 10 }, 0.25, 'bezier-stretched-linear').x.toFixed(
-      1
-    )
+    interpolate(
+      { x: 0 },
+      { x: 10 },
+      0.25,
+      'bezier-stretched-linear' as EasingKey
+    ).x.toFixed(1)
   )
   expect(interpolate({ x: 0 }, { x: 10 }, 0.5, 'linear').x.toFixed(1)).toEqual(
-    interpolate({ x: 0 }, { x: 10 }, 0.5, 'bezier-stretched-linear').x.toFixed(
-      1
-    )
+    interpolate(
+      { x: 0 },
+      { x: 10 },
+      0.5,
+      'bezier-stretched-linear' as EasingKey
+    ).x.toFixed(1)
   )
   expect(interpolate({ x: 0 }, { x: 10 }, 0.75, 'linear').x.toFixed(1)).toEqual(
-    interpolate({ x: 0 }, { x: 10 }, 0.75, 'bezier-stretched-linear').x.toFixed(
-      1
-    )
+    interpolate(
+      { x: 0 },
+      { x: 10 },
+      0.75,
+      'bezier-stretched-linear' as EasingKey
+    ).x.toFixed(1)
   )
 })
 
 test('can remove a bezier easing curve', () => {
   setBezierFunction('bezier-linear', 0, 0, 1, 1)
   unsetBezierFunction('bezier-linear')
-  expect(!Tweenable.prototype['bezier-linear']).toBeTruthy()
+  expect(Tweenable.formulas['bezier-linear' as EasingKey]).toBeUndefined()
 })
 
 test('bezier handle positions are stored on a custom easing function', () => {
